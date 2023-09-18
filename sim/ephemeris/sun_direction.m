15ff153981160657715d56bf8e8a6090007c7c25
Mingde Yin <mingdeyin@gmail.com>
2023-09-18 18:31:28 -0400

    obliquity of the sun

--- sim/ephemeris/sun_direction.m
+++ sim/ephemeris/sun_direction.m
@@ -1,6 +1,6 @@
 function dir = sun_direction(t)
 % Returns a 3D unit vector of sun direction
 phi = sun_angle(t);
-dir = [cos(phi); sin(phi); 0];
+dir = [cos(phi); sin(phi); deg2rad(23.5) * sin(phi)];
 
 end
