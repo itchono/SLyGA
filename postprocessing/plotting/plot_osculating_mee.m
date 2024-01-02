cf98250abecf5f09d70c12b30fed748d4ec4d979
Mingde Yin <mingdeyin@gmail.com>
2024-01-01 20:42:31 -0500

    animated plot improvement

--- postprocessing/plotting/plot_osculating_mee.m
+++ postprocessing/plotting/plot_osculating_mee.m
@@ -12,21 +12,24 @@ num_orbits = length(ind_orbits);
 
 %% Initial plot stuff
 fh = figure;
-plot_orbit_mee(y)
 
-L_sample = linspace(0, 2*pi, 200);
+L_sample = linspace(0, 2*pi, 100);
 plot_sphere(0, 0, 0, 6378e3, [0.3010, 0.7450, 0.9330]);
 hold on
 
+cm = colormap("turbo");
 th = title(sprintf("Orbit 1 of %d", num_orbits));
 
+% plot initial trace
 cart_sample = mee2cartesian(p(1), f(1), g(1), h(1), k(1), L_sample);
-plot3(cart_sample(1, :), cart_sample(2, :), cart_sample(3, :), "Color", "#EFA42C", "DisplayName", "Initial Orbit", "LineWidth", 2, "LineStyle", ":");
+plot3(cart_sample(1, :), cart_sample(2, :), cart_sample(3, :), "Color", cm(1, :), "DisplayName", "Initial Orbit", "LineWidth", 1, "LineStyle", "--");
 
-main_orbit = plot3(cart_sample(1, :), cart_sample(2, :), cart_sample(3, :), "Color", "black", "DisplayName", "Current Orbit", "LineWidth", 5);
+% plot main orbit
+main_orbit = plot3(cart_sample(1, :), cart_sample(2, :), cart_sample(3, :), "Color", "black", "DisplayName", "Current Orbit", "LineWidth", 1);
 
+% plot final trace
 cart_sample = mee2cartesian(p(end), f(end), g(end), h(end), k(end), L_sample);
-plot3(cart_sample(1, :), cart_sample(2, :), cart_sample(3, :), "Color", "#27CC53", "DisplayName", "Final Orbit", "LineWidth", 2, "LineStyle", ":");
+plot3(cart_sample(1, :), cart_sample(2, :), cart_sample(3, :), "Color", cm(end, :), "DisplayName", "Final Orbit", "LineWidth", 1, "LineStyle", "--");
 
 axis equal
 view(3)
@@ -38,13 +41,18 @@ open(myVideo)
 
 %% Animation
 F(num_orbits) = struct('cdata', [], 'colormap', []);
-for j = 1:num_orbits
+for j = 1:3:num_orbits
     % Update plots
     idx = ind_orbits(j);
     cart_sample = mee2cartesian(p(idx), f(idx), g(idx), h(idx), k(idx), L_sample);
     main_orbit.XData = cart_sample(1, :);
     main_orbit.YData = cart_sample(2, :);
     main_orbit.ZData = cart_sample(3, :);
+
+    % Add shadow of previous orbits
+    colour_idx = ceil(j/num_orbits * length(cm));
+    plot3(cart_sample(1, :), cart_sample(2, :), cart_sample(3, :), "Color", cm(colour_idx, :), "DisplayName", "Current Orbit", "LineWidth", 0.5);
+
     th.String = sprintf("Orbit %d of %d", j, num_orbits);
 
     % Draw and get frame
--- postprocessing/postprocess.m
+++ postprocessing/postprocess.m
@@ -15,17 +15,17 @@ writestruct(exportable_cfg, fullfile("outputs", casename, 'mission_config.xml'))
 %% Figures
 hf1 = figure;
 plot_elements_mee(y, t, y_target);
-saveas(hf1, fullfile("outputs", casename, 'slyga_elements.pdf'))
+saveas(hf1, fullfile("outputs", casename, 'orbital_elements.pdf'))
 
 hf2 = figure;
 plot_steering_history(y, t, mission_cfg);
-saveas(hf2, fullfile("outputs", casename, 'slyga_steering.pdf'))
+saveas(hf2, fullfile("outputs", casename, 'steering_history.pdf'))
 
 hf3 = figure;
 [y_interp, ~] = interp_mee(y, t, 100);
 plot_orbit_mee(y_interp);
-exportgraphics(hf3, fullfile("outputs", casename, 'slyga_orbit_plot.png'), ...
+exportgraphics(hf3, fullfile("outputs", casename, 'trajectory_plot.png'), ...
     'Resolution', 300)
 
 %% Video
-plot_osculating_mee(y, fullfile("outputs", casename, 'slyga_video.mp4'));
+plot_osculating_mee(y, fullfile("outputs", casename, 'trajectory_animation.mp4'));
