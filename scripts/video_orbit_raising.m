0b78cd3b585b3a3731b6a30a0aa1b0fd8bc188f4
Mingde Yin <mingdeyin@gmail.com>
2024-03-27 23:58:23 -0400

    presentation demos

---
+++ scripts/video_orbit_raising.m
@@ -0,0 +1,27 @@
+%% Description
+% Video demo
+
+%% Problem Definition
+% Create a struct for neatness
+cfg.y0 = [10000e3; 0; -0.1; 0; 0; 0];
+cfg.y_target = [20000e3; 0; -0.7; 0; 0];
+cfg.propulsion_model = @constant_thrust;
+cfg.steering_law = @lyapunov_steering;
+cfg.solver = @ode89;
+cfg.t_span = [0, 1e7];
+cfg.options = odeset('RelTol', 1e-4, "Stats", "on", "MaxStep", 1e4);
+cfg.tol = 5e-3;
+cfg.guidance_weights = [1; 1; 1; 1; 1];
+cfg.penalty_param = 1;
+cfg.min_pe = 10000e3;
+cfg.penalty_weight = 0;
+cfg.kappa_d = deg2rad(90);
+cfg.kappa_f = deg2rad(91);
+cfg.dynamics = "mee";
+cfg.j2 = false;
+
+%% Run
+[~, cfg.casename, ~] = fileparts(mfilename);
+[y, t, dv] = run_mission(cfg);
+
+imseq_osculating_mee(y, "anim", 2)
