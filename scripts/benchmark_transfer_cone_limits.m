7484bd9737afcb9692457e033343ac3f6c0a9bae
Mingde Yin <mingdeyin@gmail.com>
2024-04-01 02:04:19 -0400

    new cases for presentation, new fancy export

---
+++ scripts/benchmark_transfer_cone_limits.m
@@ -0,0 +1,25 @@
+%% Description
+% cone limits
+
+%% Problem Definition
+% Create a struct for neatness
+cfg.y0 = [20000e3; 0.5; -0.2; 0.5; 0; 0];
+cfg.y_target = [25000e3; 0.2; 0.5; 0; 0.3];
+cfg.propulsion_model = @sail_thrust;
+cfg.steering_law = @lyapunov_steering;
+cfg.solver = @ode89;
+cfg.t_span = [0, 1e8];
+cfg.options = odeset('RelTol', 1e-4, "Stats", "on", "MaxStep", 1e4);
+cfg.tol = 1e-3;
+cfg.guidance_weights = [1; 1; 1; 1; 1];
+cfg.penalty_param = 1;
+cfg.min_pe = 10000e3;
+cfg.penalty_weight = 0;
+cfg.kappa_d = deg2rad(50);
+cfg.kappa_f = deg2rad(91);
+cfg.dynamics = "mee";
+cfg.j2 = false;
+
+%% Run
+[~, cfg.casename, ~] = fileparts(mfilename);
+[y, t, dv] = run_mission(cfg);
