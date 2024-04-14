f8b363d238f997edcb8a3ab565cddcccd2150b8b
Mingde Yin <mingdeyin@gmail.com>
2024-04-13 22:32:13 -0400

    mission summary QOL improvements

--- postprocessing/printing/print_mission_summary.m
+++ postprocessing/printing/print_mission_summary.m
@@ -14,14 +14,14 @@ FIDS = [1, fopen(fullfile("outputs", cfg.casename, 'mission_summary.txt'), 'w')]
 for i = 1:2
     fid = FIDS(i);
     fprintf(fid, "FINISHED in %.2f seconds (compute)\n", time_elapsed);
-
+    
     if final_err < cfg.tol
         fprintf(fid, "   _____ ____  _   ___      ________ _____   _____ ______ _____  _ \n  / ____/ __ \\| \\ | \\ \\    / /  ____|  __ \\ / ____|  ____|  __ \\| |\n | |   | |  | |  \\| |\\ \\  / /| |__  | |__) | |  __| |__  | |  | | |\n | |   | |  | | . ` | \\ \\/ / |  __| |  _  /| | |_ |  __| | |  | | |\n | |___| |__| | |\\  |  \\  /  | |____| | \\ \\| |__| | |____| |__| |_|\n  \\_____\\____/|_| \\_|   \\/   |______|_|  \\_\\\\_____|______|_____/(_)\n");
     else
         fprintf(fid, "NOT CONVERGED!!\n");
     end
-    fprintf(fid, "Achieved error of %g (vs tolerance %g) after %d orbits (%.f seconds) with %d solver steps\n", ...
-        final_err, cfg.tol, num_orbits, t(end), length(t));
+    fprintf(fid, "Achieved error of %g (vs tolerance %g) after %d orbits (%.f seconds = %.0f days) with %d solver steps\n", ...
+        final_err, cfg.tol, num_orbits, t(end), t(end)/86400, length(t));
     
     fprintf(fid, "Total DV expenditure: %.1f m/s\n", dv(end));
 end
